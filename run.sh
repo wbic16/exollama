f7714a80fe389f863262427e3e302a921e7ec980
Will Bickford <wbic16@gmail.com>
2025-01-01 16:10:24 -0600

    Going Phext Native

--- run.sh
+++ run.sh
@@ -3,24 +3,24 @@
 #
 # OUTPUT = output.txt: a simple data collection pipeline
 # TEMP = 0.4: Prefer rational outputs, slightly
-# STEPS = 1024: Each step produces about 4 bytes of text, so 512 steps = 2 KB
+# STEPS = 512: Each step produces about 4 bytes of text, so 512 steps = 2 KB
 
-OUTPUT="output.txt"
+OUTPUT="output.phext"
 TEMP=0.4
-STEPS=2048
+STEPS=512
 
 ./setup.sh
 file=$1
 if [ "x$file" = "x" ]; then
-  file="prompt.txt"
+  file="prompt.phext"
 fi
 if [ ! -f $file ]; then
   echo "You should store your prompt in $file and try again."
   exit 1
 fi
-PROMPT_BYTES=`wc --bytes $file |sed 's/ .*$//g'`
-PROMPT_STEPS=$(($PROMPT_BYTES*0.25))
-STEPS=$(($STEPS + $PROMPT_STEPS))
+#PROMPT_BYTES=`wc --bytes $file |sed 's/ .*$//g'`
+#PROMPT_STEPS=$(($PROMPT_BYTES/4))
+#STEPS=$(($STEPS + $PROMPT_STEPS))
 cd build
 if [ ! -f ./llama2_q4 ]; then
   echo "Setup Error - review output above."
