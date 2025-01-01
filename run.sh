ac4a7af6b1e53ac8bce178187abdd93af2bbdb02
Will Bickford <wbic16@gmail.com>
2025-01-01 16:26:59 -0600

    Packaging v0.0.1

--- run.sh
+++ run.sh
@@ -2,12 +2,16 @@
 # exollama runner
 #
 # OUTPUT = output.txt: a simple data collection pipeline
-# TEMP = 0.4: Prefer rational outputs, slightly
+# TEMP = 0.53: Prefer creative outputs, slightly (default: 0.5)
+# SAMP = 0.73: Prefer less-random sampling than stock (default: 0.9)
 # STEPS = 512: Each step produces about 4 bytes of text, so 512 steps = 2 KB
+# SECS = how long we expect the process to run (baselined on an i9-13900HX)
 
 OUTPUT="output.phext"
-TEMP=0.4
-STEPS=512
+TEMP=0.53
+SAMP=0.73
+SECS=15
+STEPS=$(($SECS*85))
 
 ./setup.sh
 file=$1
@@ -25,7 +29,7 @@ cd build
 if [ ! -f ./llama2_q4 ]; then
   echo "Setup Error - review output above."
 else
-  ./llama2_q4 ./llama2-7b-awq-q4.bin -n $STEPS -t $TEMP -f ../$file >../$OUTPUT
+  ./llama2_q4 ./llama2-7b-awq-q4.bin -n $STEPS -t $TEMP -p $SAMP -f ../$file >../$OUTPUT
 fi
 cd ../
 cat $OUTPUT
