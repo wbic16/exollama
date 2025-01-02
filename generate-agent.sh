4b2528baf9cb9143bc421fd98126a6df0d2da415
Will Bickford <wbic16@gmail.com>
2025-01-01 20:51:48 -0600

    Added some error checking and restarts

--- generate-agent.sh
+++ generate-agent.sh
@@ -5,6 +5,14 @@ while [ $x -lt 100 ]; do
   x=$(($x+1))
   if [ -f agent.$x ]; then
     echo "Agent $x: OK"
+    if [ $x -gt 1 ]; then
+      ./verify-agent.sh $x
+      if [ $? -ne 0 ]; then
+	echo "Re-generating Agent $x..."
+	rm -f "agent.$x"
+        x=$(($x-2))
+      fi
+    fi
   else
     y=$(($x-1))
     echo "Generating Agent $x from Agent $y..."
---
+++ verify-agent.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+ID=$1
+if [ "x$ID" = "x" ]; then
+  echo "USage: $0 <id>"
+  exit 1
+fi
+if [ ! -f "agent.$ID" ]; then
+  echo "No agent definition for $ID yet."
+  exit 1
+fi
+FILE="agent.$ID"
+CONTENT=`cat $FILE`
+NAME=`cat $FILE |grep 'Name:' |sed 's/Name: //g'`
+BORN=`cat $FILE |grep 'Born:' |sed 's/Born: //g'`
+BIO=`cat $FILE |grep 'Bio:' |sed 's/Bio: //g'`
+VERIFIED=0
+NAME_CHECK=`echo "$NAME" |sed 's/[a-zA-Z]//g'`
+BORN_CHECK=`echo "$BORN" |sed 's/[a-zA-Z]//g'`
+BIO_CHECK=`echo "$BIO" |sed 's/[a-zA-Z]//g'`
+if [ ${#NAME} -gt ${#NAME_CHECK} ]; then VERIFIED=$(($VERIFIED+1)); fi
+if [ ${#BORN} -gt ${#BORN_CHECK} ]; then VERIFIED=$(($VERIFIED+1)); fi
+if [ ${#BIO}  -gt ${#BIO_CHECK}  ]; then VERIFIED=$(($VERIFIED+1)); fi
+echo "Name: $NAME"
+echo "Born: $BORN"
+echo "Bio: $BIO"
+if [ $VERIFIED -eq 3 ]; then echo "Verified."; exit 0; else echo "Failed."; exit 1; fi
