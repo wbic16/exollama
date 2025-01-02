68755f7e3b223d4e1c09a5f10f9c4663868d9e05
Will Bickford <wbic16@gmail.com>
2025-01-01 22:05:49 -0600

    Reviewed agents 8-9

--- reset.sh
+++ reset.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-dd if=/dev/urandom of=rando.phext bs=1024 count=2
-hexdump rando.phext >rando.phext
+dd if=/dev/urandom of=rando bs=1024 count=2
+hexdump rando >rando.phext
+rm -f rando
 ./run.sh rando.phext
