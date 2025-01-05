2c62ed5cceb21e551e9b07c0bcf00b1e07e2a262
Will Bickford <wbic16@gmail.com>
2025-01-04 23:28:02 -0600

    Added a missing agent lookup

--- list-agents.sh
+++ list-agents.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 AGENT=1
+MODE=$1
+if [ "x$MODE" = "x" ]; then
+  MODE="exists"
+fi
 while [ $AGENT -lt 100 ]; do
-  if [ -f ./agents/agent.$AGENT ]; then
-    ./verify-agent.sh $AGENT |head -1 |sed 's/Name: /Agent #'$AGENT': /g'
+  if [ "x$MODE" = "xexists" ]; then
+    if [ -f ./agents/agent.$AGENT ]; then
+      ./verify-agent.sh $AGENT |head -1 |sed 's/Name: /Agent #'$AGENT': /g'
+    fi
+  fi
+  if [ "x$MODE" = "xmissing" ]; then
+    if [ ! -f ./agents/agent.$AGENT ]; then
+      echo "Agent #$AGENT"
+    fi
   fi
   AGENT=$(($AGENT + 1))
 done
