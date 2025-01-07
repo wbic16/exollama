57c690bcd243605abe1fa2e37b197c2f6e16dead
Will Bickford <wbic16@gmail.com>
2025-01-07 00:15:10 -0600

    Kaelin Thatcher chooses Chronos

--- epochs/epoch-1/agent-step.sh
+++ epochs/epoch-1/agent-step.sh
@@ -2,5 +2,5 @@
 ID=`cat id`
 NAME=`cd ../../ && ./verify-agent.sh $ID |grep 'Name:' |sed 's/Name: //g'`
 echo "Taking next time-step action for $NAME (Agent #$ID)..."
-ollama run tinyllama "Choose your pod from this list: aurora, chronos, elysium, helios, nyx, or tyche. Assume the identity of $NAME, Agent #$ID." >output.phext
+ollama run tinyllama "Don't over-think this. Work step by step. Choose your pod from this list: aurora, chronos, elysium, helios, nyx, or tyche. Assume the identity of $NAME, Agent #$ID." >output.phext
 cat output.phext
