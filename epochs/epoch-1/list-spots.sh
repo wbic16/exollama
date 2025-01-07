e796471f0706533fcca9e82728f3f3d137a55e1a
Will Bickford <wbic16@gmail.com>
2025-01-07 01:28:54 -0600

    Ignore fate for pod spots too

--- epochs/epoch-1/list-spots.sh
+++ epochs/epoch-1/list-spots.sh
@@ -6,6 +6,7 @@ for FILE in `ls -1 *.phext`; do
   SHOW=1
   if [ "x$LABEL" = "xjobs" ]; then SHOW=0; fi
   if [ "x$LABEL" = "xoutput" ]; then SHOW=0; fi
+  if [ "x$LABEL" = "xfate" ]; then SHOW=0; fi
   if [ $SHOW -eq 1 ]; then
     echo "$LABEL: $COUNT"
   fi
