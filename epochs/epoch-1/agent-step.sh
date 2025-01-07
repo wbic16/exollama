57a8d10c876ed9bce5f8d3fe64048809515c6cd5
Will Bickford <wbic16@gmail.com>
2025-01-07 01:25:38 -0600

    Calypso Virelli Mirage chooses Nyx *and* Tyche...

--- epochs/epoch-1/agent-step.sh
+++ epochs/epoch-1/agent-step.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+MODEL="gemma:7b" # llama3.2, qwen2, gemma, tinyllama, mixtral, etc.
 ID=`cat id`
 NAME=`cd ../../ && ./verify-agent.sh $ID |grep 'Name:' |sed 's/Name: //g'`
 echo "Taking next time-step action for $NAME (Agent #$ID)..."
-ollama run llama3.2 --verbose "Think like an agent. Your first mission, should you choose to accept it, is to pick a pod from this list: aurora, chronos, elysium, helios, nyx, or tyche. Assume the identity of $NAME, Agent #$ID." >output.phext
+ollama run $MODEL --verbose "Think like an agent. Your first mission, should you choose to accept it, is to pick a pod from this list: aurora, chronos, elysium, helios, nyx, or tyche. Assume the identity of $NAME, Agent #$ID." >output.phext
 cat output.phext
 NEXT_ID=$(($ID+1))
 echo $NEXT_ID >id
