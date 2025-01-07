5f4c6be25de12d4f9246664cb447e61be03143ad
Will Bickford <wbic16@gmail.com>
2025-01-07 00:45:29 -0600

    Automating some steps

--- epochs/epoch-1/agent-step.sh
+++ epochs/epoch-1/agent-step.sh
@@ -4,3 +4,7 @@ NAME=`cd ../../ && ./verify-agent.sh $ID |grep 'Name:' |sed 's/Name: //g'`
 echo "Taking next time-step action for $NAME (Agent #$ID)..."
 ollama run llama3.2 --verbose "Think like an agent. Your first mission, should you choose to accept it, is to pick a pod from this list: aurora, chronos, elysium, helios, nyx, or tyche. Assume the identity of $NAME, Agent #$ID." >output.phext
 cat output.phext
+ID=$(($ID+1))
+echo $ID >id
+git add id
+git status
