e9fd1a9cd34c3716c26a5a1086c6086f6cb3b208
Will Bickford <wbic16@gmail.com>
2025-01-07 00:34:09 -0600

    Use llama3.2 to improve choices

--- epochs/epoch-1/agent-step.sh
+++ epochs/epoch-1/agent-step.sh
@@ -2,5 +2,5 @@
 ID=`cat id`
 NAME=`cd ../../ && ./verify-agent.sh $ID |grep 'Name:' |sed 's/Name: //g'`
 echo "Taking next time-step action for $NAME (Agent #$ID)..."
-ollama run tinyllama --verbose "Think like an agent. Your first mission, should you choose to accept it, is to pick a pod from this list: aurora, chronos, elysium, helios, nyx, or tyche. Assume the identity of $NAME, Agent #$ID." >output.phext
+ollama run llama3.2 --verbose "Think like an agent. Your first mission, should you choose to accept it, is to pick a pod from this list: aurora, chronos, elysium, helios, nyx, or tyche. Assume the identity of $NAME, Agent #$ID." >output.phext
 cat output.phext
