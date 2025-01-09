9a1aa35c596b032c3b5c60af1b2202ce5f516110
Will Bickford <wbic16@gmail.com>
2025-01-08 23:52:46 -0600

    processing ideas

---
+++ epochs/epoch-1/process.sh
@@ -0,0 +1,10 @@
+# goal: agents orbit each other
+# goal: pods produce X scrolls per day
+# perf: 5x25 tok/sec = 125 tok/sec aggregate
+#
+# aurora: 19 -> 1.3
+# chronos: 23 -> 1.08
+# elysium: 18 -> 1.3
+# helios: 11 -> 2.2
+# nyx: 24 -> 1.04
+# tyche: 6 -> 4.1
