0cceea3fc098fac3a64511090386107b1506decb
Will Bickford <wbic16@gmail.com>
2025-01-09 01:08:09 -0600

    Think, step by step

---
+++ epochs/epoch-1/think.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+ID=$1
+if [ -z $ID ]; then ID=1; fi
+COORD=`./coordinate.sh $ID`
+./get-message.sh $COORD
+LINES=`wc -l msg.txt |awk '{print $1}'`
+if [ $LINES -eq 0 ]; then
+  cp "../../agents/agent.$ID" msg.txt
+fi
+TEXT=`cat msg.txt`
+../../l2.sh "$TEXT" >msg.txt
+cat msg.txt
+./post-message.sh $COORD
