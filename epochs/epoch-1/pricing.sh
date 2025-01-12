1e96e9f5bc6f52a326f6c230a23db703853cf385
Will Bickford <wbic16@gmail.com>
2025-01-11 21:10:00 -0600

    Pricing Model 3

--- epochs/epoch-1/pricing.sh
+++ epochs/epoch-1/pricing.sh
@@ -4,9 +4,9 @@ TOK_RATING=$((25/$AGENT_COUNT))
 COMPETITION_PRICE=$(($TOK_RATING*6))
 PRICE=$((2000/($TOK_RATING*$AGENT_COUNT)))
 TPM=500 # tokens per message
-BRONZE=$(($TPM*10/$TOK_RATING/500))
-SILVER=$(($TPM*25/$TOK_RATING/750))
-GOLD=$(($TPM*100/$TOK_RATING/1000))
+BRONZE=$(($TPM*10/$TOK_RATING/1000))
+SILVER=$(($TPM*25/$TOK_RATING/1500))
+GOLD=$(($TPM*100/$TOK_RATING/2000))
 echo "Gold: 100 requests/day: \$$GOLD/mo"
 echo "Silver: 25 requests/day: \$$SILVER/mo"
 echo "Bronze: 10 requests/day: \$$BRONZE/mo"
