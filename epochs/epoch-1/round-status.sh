7042772bb755eb49011c19d8917cf6dd82d8b010
Will Bickford <wbic16@gmail.com>
2025-01-14 19:27:11 -0600

    Switch to exo as the default branch name

--- epochs/epoch-1/round-status.sh
+++ epochs/epoch-1/round-status.sh
@@ -1,10 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 ROUND=0
-EXOLLAMA_ROOT="../.."
-if [ -d "/usr/local/exollama" ]; then
-  EXOLLAMA_ROOT="/usr/local/exollama"
-fi
+source exollama.inc
 POD=`$EXOLLAMA_ROOT/pod-name.sh`
 if [ -f round ]; then ROUND=`cat round`; fi
 cat $POD.roster
-git log --oneline origin/main..main
+git log --oneline "origin/$EXOLLAMA_BRANCH..$EXOLLAMA_BRANCH"
