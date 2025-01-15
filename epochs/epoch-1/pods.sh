c1eca364021174e7e5104b3a76b5e6c9dfb35321
Will Bickford <wbic16@gmail.com>
2025-01-14 19:57:45 -0600

    Added pods

---
+++ epochs/epoch-1/pods.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source exollama.inc
+cd $EXOLLAMA_ROOT
+if [ ! -d .pods ]; then mkdir .pods; fi
+for ROW in `ls -1 agents`
+do
+  HAVE_THOUGHTS=`echo $ROW |grep -c '\.thought'`
+  if [ $HAVE_THOUGHTS -eq 0 ]; then
+    ID=`echo $ROW |sed 's/agent\.//g'`
+    AGENT=`echo $ROW |sed 's/\..*$//g'`
+    PORT=$(($ID + 11000))
+    if [ ! -d .pods/$ID ]; then
+      mkdir .pods/$ID
+    fi
+    cd .pods/$ID
+    sq $PORT >output.txt 2>&error.txt &
+    cd ../..
+  fi
+done
