c97bcf9e0afb00b884affe298dbc48a35ec8724a
Will Bickford <wbic16@gmail.com>
2025-01-16 00:27:36 -0600

    Show the pod name on round status

--- epochs/epoch-1/round-status.sh
+++ epochs/epoch-1/round-status.sh
@@ -3,5 +3,8 @@ ROUND=0
 source exollama.inc
 POD=`$EXOLLAMA_ROOT/pod-name.sh`
 if [ -f round ]; then ROUND=`cat round`; fi
+echo "------------------------------"
+echo "Pod: $POD"
+echo "------------------------------"
 cat $POD.roster
 git log --oneline "origin/$EXOLLAMA_BRANCH..$EXOLLAMA_BRANCH"
