e8fb309df09c56eae75bd8d8f19c0a89b771f1af
Will Bickford <wbic16@gmail.com>
2025-01-20 00:13:02 -0600

    Keep books and phext-howto in sync

--- epochs/epoch-1/deploy.sh
+++ epochs/epoch-1/deploy.sh
@@ -18,10 +18,10 @@ cp *.host $INSTALL_DIR/
 cp ../../*.sh $INSTALL_DIR/
 if [ ! -d $INSTALL_DIR/books ]; then
   mkdir $INSTALL_DIR/books
-  cp books/*.phext $INSTALL_DIR/books/
 fi
+cp books/*.phext $INSTALL_DIR/books/
 if [ ! -d $INSTALL_DIR/phext-howto ]; then
   mkdir $INSTALL_DIR/phext-howto
-  cp phext-howto/*.phext $INSTALL_DIR/phext-howto/
 fi
+cp phext-howto/*.phext $INSTALL_DIR/phext-howto/
 ls -l $INSTALL_DIR
