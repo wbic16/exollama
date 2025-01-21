6ed65d3bc4dfe3a9ec8d2d2150646f6c533f3888
Will Bickford <wbic16@gmail.com>
2025-01-20 21:52:21 -0600

    Support dual-execution modes like other scripts

--- epochs/epoch-1/chain.sh
+++ epochs/epoch-1/chain.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+source exollama.inc
 echo "Thinking..."
-P1=`../../dq.sh "$1"`
+P1=`$EXOLLAMA_ROOT/dq.sh "$1"`
 echo "$P1"
 echo "Refining..."
-P2=`../../dq.sh "$P1"`
+P2=`$EXOLLAMA_ROOT/dq.sh "$P1"`
 echo "$P2"
 echo "Considering..."
-P3=`../../dq.sh "$P2"`
+P3=`$EXOLLAMA_ROOT/dq.sh "$P2"`
 echo "$P3"
 echo "Finalizing..."
-P4=`../../dq.sh "$P3"`
+P4=`$EXOLLAMA_ROOT/dq.sh "$P3"`
 echo $P4
