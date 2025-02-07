1f1bf77e6fdc04a4ae649fb7f2c20467e20fd4a5
Will Bickford <wbic16@gmail.com>
2025-02-06 22:06:52 -0600

    Added article checkout and the creative commons license

---
+++ epochs/epoch-1/check-article.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ARTICLE="$1"
+FILE="article.txt"
+if [ "x$2" != "x" ]; then
+  FILE="$2"
+fi
+COORDINATE=`./wiki-coordinate.sh "$ARTICLE"`
+SQ_RUNNING=`ps -ef |grep -v grep |grep -c "sq wikipedia.phext"`
+if [ $SQ_RUNNING -eq 0 ]; then
+  rm -rf .sq
+  sq wikipedia.phext &
+fi
+FOUND=`sq toc |grep -c $COORDINATE`
+if [ $FOUND -eq 0 ]; then
+  echo "No entry for '$ARTICLE' => $COORDINATE found."
+else
+  sq pull $COORDINATE $FILE
+  echo "Fetched $ARTICLE into $FILE."
+fi
